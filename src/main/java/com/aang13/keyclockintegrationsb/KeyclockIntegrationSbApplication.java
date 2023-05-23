bf34acb78d9352f9eaa88279aebb501bafeff2a5
moinul <mdmoinulh13@gmail.com>
2023-05-23 14:05:51 +0600

    project set up

---
+++ src/main/java/com/aang13/keyclockintegrationsb/KeyclockIntegrationSbApplication.java
@@ -0,0 +1,13 @@
+package com.aang13.keyclockintegrationsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyclockIntegrationSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyclockIntegrationSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aang13/keyclockintegrationsb/KeyclockIntegrationSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aang13.keyclockintegrationsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyclockIntegrationSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
